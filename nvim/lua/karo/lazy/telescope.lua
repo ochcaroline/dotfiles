81039a082a9931e9367b2d270ad7730a54564aec
ochcaroline <28461795+ochcaroline@users.noreply.github.com>
2025-12-25 20:20:43 +0100

    Update brewfile, update telescope keymaps

--- nvim/lua/karo/lazy/telescope.lua
+++ nvim/lua/karo/lazy/telescope.lua
@@ -35,5 +35,9 @@ return {
 		vim.keymap.set("n", "<leader><leader>", get_files, { desc = "Get git files or all files if not git repo" })
 		vim.keymap.set("n", "<leader>/", builtin.live_grep, {})
 		vim.keymap.set("n", "gr", builtin.lsp_references, {})
+		vim.keymap.set("n", "gd", builtin.lsp_definitions, {})
+		vim.keymap.set("n", "gf", function()
+			builtin.lsp_document_symbols({ symbols = { "function", "class", "method", "struct" } })
+		end, {})
 	end,
 }
